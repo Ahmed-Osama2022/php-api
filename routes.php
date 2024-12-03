1f8e13efa734ad315a7d65e78ecc6113da2dd5c0
ahmed-osama2022 <ahmadosama.2011@gmail.com>
2024-12-03 09:28:32 +0200

    Add more comments on "routes.php" file

--- routes.php
+++ routes.php
@@ -4,10 +4,15 @@
  * Here define your routes as the following
  * Methods available => "GET" ,"POST", "PUT", "DELETE" => "CRUD operations"
  * $router->method('path', 'yourController@methodIntheController', ['middlewareTypeInYourMiddlewareFramework']);
- *
+ * ----------------------------------------------------------------------------
  * e.g.
  * $router->get('/', 'HomeController@index', ['guest']);
+ * ----------------------------------------------------------------------------- 
+ * For Params
+ * e.g.
+ * $router->get('/products/{id}', 'HomeController@edit');
  */
-
+// For the TEST
 $router->get('/', 'HomeController@index', ['guest']);
 $router->get('/test', 'HomeController@test', ['guest']);
+// Start making your routes here...
