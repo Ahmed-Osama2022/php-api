bbdbb88fd42bd78e44558637a4ac99250fe15800
ahmed-osama2022 <ahmadosama.2011@gmail.com>
2024-12-03 10:15:30 +0200

    Add more comments.

--- routes.php
+++ routes.php
@@ -1,18 +1,47 @@
 <?php
 
 /**
- * Here define your routes as the following
- * Methods available => "GET" ,"POST", "PUT", "DELETE" => "CRUD operations"
- * $router->method('path', 'yourController@methodIntheController', ['middlewareTypeInYourMiddlewareFramework']);
- * ----------------------------------------------------------------------------
- * e.g.
- * $router->get('/', 'HomeController@index', ['guest']);
- * ----------------------------------------------------------------------------- 
- * For Params
- * e.g.
- * $router->get('/products/{id}', 'HomeController@edit');
+ * | Here define your routes as the following
+ * | Methods available => "GET" ,"POST", "PUT", "DELETE" => "CRUD operations"
+ * | $router->method('path', 'yourController@methodIntheController', ['middlewareTypeInYourMiddlewareFramework']);
+ * | ----------------------------------------------------------------------------
+ * | e.g.
+ * | $router->get('/', 'HomeController@index', ['guest']);
+ * | ----------------------------------------------------------------------------- 
+ * | For Params
+ * | e.g.
+ * | $router->get('/products/{id}', 'HomeController@edit');
+ */
+
+/**
+ * | -------------------------------------------------------------------
+ * | ------------------------------ GET --------------------------------
+ * | -------------------------------------------------------------------
  */
 // For the TEST
 $router->get('/', 'HomeController@index', ['guest']);
 $router->get('/test', 'HomeController@test', ['guest']);
-// Start making your routes here...
+
+// For the APP.
+
+/**
+ * | -------------------------------------------------------------------
+ * | ------------------------------ POST -------------------------------
+ * | -------------------------------------------------------------------
+ */
+
+
+
+/**
+ * | -------------------------------------------------------------------
+ * | ----------------------------- PATCH -------------------------------
+ * | -------------------------------------------------------------------
+ */
+
+
+
+/**
+ * | -------------------------------------------------------------------
+ * | ---------------------------- DELETE -------------------------------
+ * | -------------------------------------------------------------------
+ */
